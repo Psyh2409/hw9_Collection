a1d61e8dcb903aef081235d16c68ec223c92888c
Psyh2409 <psyh2409@gmail.com>
2020-10-04 19:00:52 +0300

    copying files method for task 1

--- src/com/gmail/psyh2409/task1/DocFiles.java
+++ src/com/gmail/psyh2409/task1/DocFiles.java
@@ -1,6 +1,10 @@
 package com.gmail.psyh2409.task1;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
 
 public class DocFiles {
     private String path;
@@ -28,5 +32,29 @@ public class DocFiles {
         if (files == null) return new File[0];
         if (files.length == 0) System.out.println(path + " hasn't files with extension '" + extension + "'.");
         return files;
+    }
+
+    public void copyFiles(File dir, File[] files) {
+        if (dir == null || files == null) {
+            System.out.println("Folder or files is null.");
+            return;
+        } else {
+            if (!dir.exists() & dir.mkdir()) {
+                for (File file : files) {
+                    if (file != null) {
+                        try {
+                            Path fileForCopy = file.toPath();
+                            Path dirCopyTo = new File(
+                                    dir.toString().concat(
+                                            fileForCopy.toString().substring(
+                                                    fileForCopy.toString().lastIndexOf(File.separator)))).toPath();
+                            Files.copy(fileForCopy, dirCopyTo, StandardCopyOption.REPLACE_EXISTING);
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
     }
 }
